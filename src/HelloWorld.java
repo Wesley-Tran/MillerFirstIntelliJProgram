f7db1c71fb07dbe5a765dd5f5d095cdbd90ac0c1
Wesley Tran <sample@bths.edu>
2022-09-22 11:12:51 -0400

    added hello world 9/22

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("changed a print statement in the code");
+
+        System.out.println("Hello World again!");
     }
 }
